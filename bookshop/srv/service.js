2739cb553b4e4ae6ff80c2e5cc88d2cca58d453d
Marco Silva <marco.pucp@gmail.com>
2025-03-11 04:01:45 +0000

    add bookshop

---
+++ bookshop/srv/service.js
@@ -0,0 +1,12 @@
+    /**
+    * Implementation for bookshopService defined in ./service.cds
+    **/
+    const cds = require('@sap/cds')
+    module.exports = function (){
+      // Register your event handlers in here, e.g....
+      this.after ('READ','Books', each => {
+        if (each.stock > 80) {
+          each.title += ` -- 11% discount!`
+        }
+      })
+    }
