7f0a885326e926f81f88ec965e44e9bab8a23882
Edmundo Solis <dante.solis@gmail.com>
2025-03-18 01:15:05 +0000

    tema 5

---
+++ incident-management/app/incides/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ns.incides.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
