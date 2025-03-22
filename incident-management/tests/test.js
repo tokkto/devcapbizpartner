89369051b22178608c78862c2581f17688bd6ca6
Carlos Quispe <cquispep@gmail.com>
2025-03-22 22:14:23 +0000

    add testcq

--- incident-management/tests/test.js
+++ incident-management/tests/test.js
@@ -10,13 +10,13 @@ describe('Test The GET Endpoints', () => {
   it('Should check Processor Service', async () => {
     const processorService = await cds.connect.to('ProcessorService')
     const { Incidents } = processorService.entities
-    expect(await SELECT.from(Incidents)).to.have.length(4)
+    expect(await SELECT.from(Incidents)).to.have.length(6)
   })
 
   it('Should check Customers', async () => {
     const processorService = await cds.connect.to('ProcessorService')
     const { Customers } = processorService.entities
-    expect(await SELECT.from(Customers)).to.have.length(3)
+    expect(await SELECT.from(Customers)).to.have.length(4)
   })
 
   it('Test Expand Entity Endpoint', async () => {
